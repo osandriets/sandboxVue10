748973b848289744fede6bec095e2c8de2fd17c3
osadnriets@gmail.com <gjx2890-12345>
2017-11-13 21:55:58 +0200

    first commit

---
+++ src/components/Green.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <p class="text-success">Text green</p>
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+    div {
+        border: 1px solid green;
+        background-color: lightgreen;
+        padding: 30px;
+        margin: 20px auto;
+        text-align: center
+    }
+</style>
---
+++ src/components/Red.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <p class="text-danger">Text Error</p>
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+    div {
+        border: 1px solid red;
+        background-color: lightcoral;
+        padding: 30px;
+        margin: 20px auto;
+        text-align: center
+    }
+</style>
